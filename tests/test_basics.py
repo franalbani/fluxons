80786a25a7ce8d8e5464cf01a8ba1ee158fe9216
Francisco Albani <franalbani@users.noreply.github.com>
2019-03-17 23:13:55 -0300

    adds consistency tests

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -7,32 +7,64 @@ from fluxon import Graph, Node, Edge
 
 class Instantiation(TestCase):
 
-    def _test_instance(self, c, *args):
+    def _test_instance(self, class_, *args):
         try:
-            c(*args)
+            class_(*args)
         except Exception as e:
             self.fail('%r was raised' % e)
-
 
     def test_graph(self):
         self._test_instance(Graph)
 
-
     def test_node(self):
         self._test_instance(Node, 'some_name', 'some_owner')
 
-
     def test_edge(self):
         self._test_instance(Edge, 'some_name', 'some_owner')
-
 
     def test_node_creation_on_first_ref(self):
         g = Graph()
         g.fran
         self.assertIn('fran', map(lambda n: n.name, g))
 
-
     def test_edge_creation_on_first_ref(self):
         g = Graph()
         g.fran.writes.tests
         self.assertIn('writes', map(lambda e: e.name, g.fran))
+
+
+class Consistency(TestCase):
+
+    def test_node_owner(self):
+        g = Graph()
+        g.fran
+        self.assertIs(g.fran.owner, g)
+
+    def test_edge_owner(self):
+        g = Graph()
+        g.fran.writes.code
+        edge = g.fran[0]
+        self.assertIs(edge.owner, g)
+
+    def test_node_chained_owner(self):
+        g = Graph()
+        g.fran.writes.code
+        self.assertIs(g.code.owner, g)
+
+    def test_node_uniqueness(self):
+        g = Graph()
+        g.fran
+        g.fran
+        self.assertEqual(sum(map(lambda n: n.name == 'fran', g)), 1)
+
+    def test_edge_non_uniqueness(self):
+        g = Graph()
+        g.fran.writes.code
+        g.fran.writes.tests
+        self.assertEqual(sum(map(lambda e: e.name == 'writes', g.fran)), 2)
+
+    def test_edge_dest(self):
+        g = Graph()
+        g.fran.writes.code
+        edge = g.fran[0]
+        self.assertIs(edge.dest, g.code)
